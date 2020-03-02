0f1de2b017cd1832d9c8dc190d169043d5c45801
hungvutuan <vutuanhung98@gmail.com>
2020-03-02 14:02:17 +0700

    Move the shipping component to ./app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,7 @@ import { ProductAlertsComponent } from './product-alerts/product-alerts.componen
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { CartService } from './cart.service';
 import { CartComponent } from './cart/cart.component';
-import { ShippingComponent } from './cart/shipping/shipping.component';
+import { ShippingComponent } from './shipping/shipping.component';
 
 @NgModule({
   imports: [
